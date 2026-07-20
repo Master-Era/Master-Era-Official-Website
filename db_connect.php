d52c49caec3dfbff304fa18f0e41850a0f8d5e0c
hariitsolution07-lgtm <hariitsolution07@gmail.com>
2026-07-20 14:17:04 +0530

    Update database connection parameters in db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,14 +1,12 @@
 <?php
+$host = "localhost";
+$user = "mastec2a_admin";
+$password = "M@sTer$S@haj0607";
+$database = "mastec2a_masterera";
 
-$conn = new mysqli(
-    "localhost",
-    "root",
-    "",
-    "masterera"
-);
+$conn = new mysqli($host, $user, $password, $database);
 
 if ($conn->connect_error) {
-    die("Connection Failed");
+    die("Connection failed: " . $conn->connect_error);
 }
-
 ?>
