edcba2d916192f6a40ff711d7401cab4934be51a
hariitsolution07-lgtm <hariitsolution07@gmail.com>
2026-07-20 15:11:59 +0530

    Login_process.php file Update

--- admin/login_process.php
+++ admin/login_process.php
@@ -5,18 +5,33 @@ include("../db_connect.php");
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$sql = "SELECT * FROM admin WHERE username='$username' AND password='$password'";
-$result = mysqli_query($conn, $sql);
+$stmt = $conn->prepare("SELECT * FROM admin WHERE username = ?");
+$stmt->bind_param("s", $username);
+$stmt->execute();
 
-if(mysqli_num_rows($result) == 1){
-    $_SESSION['admin'] = $username;
+$result = $stmt->get_result();
 
-    header("Location: dashboard.php");
-    exit();
-}else{
-    echo "<script>
+if ($result->num_rows > 0) {
+
+    $row = $result->fetch_assoc();
+
+    if (password_verify($password, $row['password'])) {
+
+        $_SESSION['admin'] = $row['username'];
+        header("Location: dashboard.php");
+        exit();
+
+    } else {
+        echo "<script>
             alert('Invalid Username or Password');
             window.location='login.php';
-          </script>";
+        </script>";
+    }
+
+} else {
+    echo "<script>
+        alert('Invalid Username or Password');
+        window.location='login.php';
+    </script>";
 }
 ?>
