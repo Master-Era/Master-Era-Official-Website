4c18400d365e3512f126469e91f7dbdf988a8227
hariitsolution07-lgtm <hariitsolution07@gmail.com>
2026-07-20 14:55:18 +0530

    Update database password in db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
 $user = "mastec2a_admin";
-$password = "M@sTerS@haj0607";
+$password = "d@TaM@ster06047";
 $database = "mastec2a_masterera";
 
 $conn = new mysqli($host, $user, $password, $database);
