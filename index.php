592783cb7a4e0a4a0f5042aaab0b32f035513db1
ASHISH <ASHISH@ASHISH>
2026-07-22 23:51:04 +0530

    update favicon

--- index.php
+++ index.php
@@ -41,7 +41,12 @@
     <meta name="twitter:image" content="https://masterera.in/images/og-banner.jpeg">
 
     <!-- Favicon -->
-    <link rel="icon" type="image/png" href="images/main logo white.png">
+    
+<link rel="icon" href="/favicon.ico" sizes="any">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="manifest" href="/site.webmanifest">
 
     <!-- AOS -->
     <link href="https://unpkg.com/aos@2.3.4/dist/aos.css" rel="stylesheet">
