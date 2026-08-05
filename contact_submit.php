029b0c9aa414e5ce53b40c9571807cdc2743fefb
hariitsolution07-lgtm <hariitsolution07@gmail.com>
2026-08-05 17:45:28 +0530

    Contact -Submmit.php Update

--- contact_submit.php
+++ contact_submit.php
@@ -4,11 +4,23 @@ ini_set('display_errors',1);
 
 include 'db_connect.php';
 
-$sql = "SELECT 1";
+$fullname  = $_POST['fullname'];
+$company   = $_POST['company'];
+$email     = $_POST['email'];
+$phone     = $_POST['phone'];
+$city      = $_POST['city'];
+$service   = $_POST['service'];
+$employees = $_POST['employees'];
+$message   = $_POST['message'];
 
-if(!$conn){
-    die($conn->connect_error);
+$sql = "INSERT INTO contact (
+fullname, company, email, phone, city, service, employees, message
+) VALUES (
+'$fullname','$company','$email','$phone','$city','$service','$employees','$message'
+)";
+
+if ($conn->query($sql)) {
+    echo "Insert Success";
+} else {
+    die("MySQL Error: " . $conn->error);
 }
-
-echo "Database Connected Successfully";
-exit;
