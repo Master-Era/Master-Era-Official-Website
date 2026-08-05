183f742ac39a87eeb1982f1c5077db89f6bf20cf
hariitsolution07-lgtm <hariitsolution07@gmail.com>
2026-08-05 17:48:31 +0530

    Login Process Php Chnage file

--- admin/login_process.php
+++ admin/login_process.php
@@ -2,21 +2,28 @@
 session_start();
 include("../db_connect.php");
 
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+
 $username = $_POST['username'];
 $password = $_POST['password'];
 
 $sql = "SELECT * FROM admin WHERE username='$username' AND password='$password'";
+
 $result = mysqli_query($conn, $sql);
+
+if (!$result) {
+    die("SQL Error: " . mysqli_error($conn));
+}
+
+echo "Query Executed<br>";
+echo "Rows Found: " . mysqli_num_rows($result) . "<br>";
 
 if(mysqli_num_rows($result) == 1){
     $_SESSION['admin'] = $username;
-
-    header("Location: dashboard.php");
-    exit();
+    echo "Login Success";
+    exit;
 }else{
-    echo "<script>
-            alert('Invalid Username or Password');
-            window.location='login.php';
-          </script>";
+    echo "Invalid Username or Password";
+    exit;
 }
-?>
