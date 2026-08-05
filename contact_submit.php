56b7971724c1be7b30dba6518fe78b7d30987cc0
hariitsolution07-lgtm <hariitsolution07@gmail.com>
2026-08-05 17:41:41 +0530

    cotact-submimit Chnage

--- contact_submit.php
+++ contact_submit.php
@@ -1,45 +1,14 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors',1);
 
 include 'db_connect.php';
 
-$fullname = $_POST['fullname'];
-$company = $_POST['company'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$city = $_POST['city'];
-$service = $_POST['service'];
-$employees = $_POST['employees'];
-$message = $_POST['message'];
+$sql = "SELECT 1";
 
-$sql = "INSERT INTO contact(
-fullname,
-company,
-email,
-phone,
-city,
-service,
-employees,
-message
-)
-VALUES(
-'$fullname',
-'$company',
-'$email',
-'$phone',
-'$city',
-'$service',
-'$employees',
-'$message'
-)";
-
-if($conn->query($sql) === TRUE){
-    echo "<script>
-    alert('Inquiry Submitted Successfully');
-    window.location='contact.html';
-    </script>";
-}
-else{
-    echo "Error";
+if(!$conn){
+    die($conn->connect_error);
 }
 
-?>
+echo "Database Connected Successfully";
+exit;
