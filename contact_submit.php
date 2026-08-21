fe4677c4609ba0e4ac43f4836f89347387e1ec6c
ASHISH <ASHISH@ASHISH>
2026-08-21 23:13:51 +0530

    Contact Submint Fale Chanages

--- contact_submit.php
+++ contact_submit.php
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-
-// error_reporting(E_ALL);
-// ini_set('display_errors',1);
-
-// include 'db_connect.php';
-
-// $fullname  = $_POST['fullname'];
-// $company   = $_POST['company'];
-// $email     = $_POST['email'];
-// $phone     = $_POST['phone'];
-// $city      = $_POST['city'];
-// $service   = $_POST['service'];
-// $employees = $_POST['employees'];
-// $message   = $_POST['message'];
-
-// $sql = "INSERT INTO contact (
-// fullname, company, email, phone, city, service, employees, message
-// ) VALUES (
-// '$fullname','$company','$email','$phone','$city','$service','$employees','$message'
-// )";
-
-// if ($conn->query($sql)) {
-//     echo "Insert Success";
-// } else {
-//     die("MySQL Error: " . $conn->error);
-// }
-
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
